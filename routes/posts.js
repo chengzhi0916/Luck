228807422e5910d26f6f5574c36a836cdc0db67f
chengzhi0916 <523183756@qq.com>
2018-12-06 21:49:50 +0800

    30

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET postss page. */
 router.get('/', function(req, res, next) {
-    res.render('posts', { title: 'Hello Word' });
+    res.render('posts', { title: 'posts',postsList:['文章一','文章二','文章三']});
   });
   
   module.exports = router;
