458933e8aa9298777a690e1e7d14f5daac9aa6ed
chengzhi0916 <523183756@qq.com>
2018-12-12 20:12:37 +0800

    30

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 var index = require('./routes/index');
 var posts = require('./routes/posts');
-var usersRouter = require('./routes/users');
+var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/posts',posts);
-app.use('/users', usersRouter);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
